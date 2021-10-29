954cb28f45219cb023cd9cc08bb2f6fb68eec5a5
Carlos <ocariocawebdesign@gmail.com>
2021-10-29 12:26:29 -0300

    Colocação de metaTags
    React helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,21 @@ import Footer from "../components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/style.css';
 //import '../css/responsive.css';
+import { Helmet } from "react-helmet";
+
+class Application extends React.Component {
+  render() {
+    return (
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>O Carioca Web Design</title>
+          <link rel="canonical" href="https://ocariocapessoalcard.gatsbyjs.io/"/>
+        </Helmet>
+      </div>
+    )
+  }
+}
 
 
 const Home = () => (
